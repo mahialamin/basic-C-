a190a57b699470ef6fc1242d2195c21784041026
mahialamin <134115827+mahialamin@users.noreply.github.com>
2023-06-18 22:17:50 +0600

    Add files via upload

---
+++ loop in array .c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+  {
+      int a[5],b[5],ab[5],i;
+
+    printf("enter the value of A: \n");
+    for(i=0;i<5;i++){
+        scanf("%d",&a[i]);
+    }
+    printf("enter the value of B: \n");
+    for(i=0;i<5;i++){
+        scanf("%d",&b[i]);
+    }
+    for(i=0;i<5;i++){
+        ab[i]=a[i]+b[i];
+    }
+    printf("vale of ab array:\n");
+    for(i=0;i<5;i++){
+    printf("%d",&ab[i]);  }
+return 0 ;
+}
